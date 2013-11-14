6f68197834e87ba107047a9fc31d23d864eb16a6
Bragge <trygglimurr@gmail.com>
2013-11-14 13:35:36 +0100

    Added differens
    mjau

--- github.cpp
+++ github.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main()
 {
-    int varde1, varde2, summa;
-    cout<<"Mata in Tv\x86 v\x84rden: ";
+    int varde1, varde2, summa, differens;
+    cout<<"hakunaMatata in Tv\x86 v\x84rden: ";
     cin>>varde1;
     cin>>varde2;
     summa=varde1+varde2;
+    differens=varde1-varde2;
     cout<<endl<<varde1<<" + "<<varde2<<" = "<<summa<<endl;
+    cout<<endl<<varde1<<" - "<<varde2<<" = "<<differens<<endl;
     
     system("PAUSE");
 }
