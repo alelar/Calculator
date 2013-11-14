3e151b5f19c2703483b5adbc5887ec40ee5c7498
Nagge <niklasrosdahl@gmail.com>
2013-11-14 13:26:19 +0100

    CoolBeans

---
+++ github.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int varde1, varde2, summa;
+    cout<<"Mata in Tv\x86 v\x84rden: ";
+    cin>>varde1;
+    cin>>varde2;
+    summa=varde1+varde2;
+    cout<<endl<<varde1<<" + "<<varde2<<" = "<<summa<<endl;
+    
+    system("PAUSE");
+}
